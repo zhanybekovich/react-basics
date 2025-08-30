de4a6fbd54e72eec3e42a6fdbaef0615b7a34329
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-30 18:40:30 +0600

    first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 
+function Greeting() {
+  return <h2>My First Component</h2>;
+}
 function App() {
   return (
     <>
       <h1>Hello React!</h1>
+      <Greeting />
     </>
   );
 }
