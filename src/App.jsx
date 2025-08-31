100ec54d57684f783bdecf2d4e5046dd4d47ad50
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-31 15:13:09 +0600

    jsx inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,15 @@ const Book = () => {
 
 const Image = () => <img src="/images/book-1.jpg" />;
 const Title = () => <h2>The Let Them Theory: A Life-Changing Tool</h2>;
-const Author = () => <h4>Mel Robbins</h4>;
+const Author = () => {
+  const inlineStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+
+  return <h4 style={inlineStyles}>Mel Robbins</h4>;
+};
 
 function App() {
   return (
