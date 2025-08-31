30905f6930206a31ad6eeaaad95f797583394773
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-31 18:36:44 +0600

    key prop

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,25 @@ const books = [
     author: "The Let Them Theory: A Life-Changing Tool",
     title: "Mel Robbins",
     image: "/images/book-1.jpg",
+    id: 1,
   },
   {
     title: "The Color of Death: A Novel",
     author: "Trey Growdy",
     image: "/images/book-2.jpg",
+    id: 2,
   },
   {
     title: "My First Learn-to-Write Workbook",
     author: "Crystal Radke",
     image: "https://m.media-amazon.com/images/I/71c+ReofeoL._SL1500_.jpg",
+    id: 3,
   },
   {
     title: "Forgotten Home Apothecary",
     author: "Dr. Nicole Apelian",
     image: "https://m.media-amazon.com/images/I/81+efMYN9wL._SL1500_.jpg",
+    id: 4,
   },
 ];
 
@@ -28,7 +32,12 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => (
-        <Book title={book.title} author={book.author} image={book.image} />
+        <Book
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          image={book.image}
+        />
       ))}
     </section>
   );
