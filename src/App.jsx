b254173ab0982dd85150e01ff0c5e128f6028938
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-31 14:41:49 +0600

    local images

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,7 @@ const Book = () => {
   );
 };
 
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL600_SR600,400_.jpg" />
-);
+const Image = () => <img src="/images/book-1.jpg" />;
 const Title = () => <h2>The Let Them Theory: A Life-Changing Tool</h2>;
 const Author = () => <h4>Mel Robbins</h4>;
 
