5c770616faadbc9b887a281f7b54a1a54be7aec0
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-31 15:36:16 +0600

    javascript in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,19 @@ const BookList = () => {
   );
 };
 
+const author = "Mel Robbins";
+
 const Book = () => {
+  const title = "The Let Them Theory: A Life-Changing Tool";
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="/images/book-1.jpg" />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      <p>{6 + 6}</p>
     </article>
   );
-};
-
-const Image = () => <img src="/images/book-1.jpg" />;
-const Title = () => <h2>The Let Them Theory: A Life-Changing Tool</h2>;
-const Author = () => {
-  const inlineStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-
-  return <h4 style={inlineStyles}>Mel Robbins</h4>;
 };
 
 function App() {
